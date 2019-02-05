543892538873eb088239a79467d41c73fef7d1b5
Ryan Haskell-Glatz <ryan.nhg@gmail.com>
2019-02-04 19:04:05 -0600

    you know what it is

--- src/Main.elm
+++ src/Main.elm
@@ -8,6 +8,7 @@ import Html
 import Html.Styled exposing (..)
 import Html.Styled.Attributes as Attr exposing (alt, css, href, id, src, target)
 import Html.Styled.Events exposing (onClick, onInput)
+import Shared
 import Styles
 import Svg.Styled as Svg
 import Svg.Styled.Attributes as SvgAttr
@@ -89,7 +90,7 @@ view model =
         , pageSection Details Nothing model
         , pageSection Speakers Nothing model
         , pageSection Sponsors (Just sponsorLogos) model
-        , siteFooter
+        , Shared.siteFooter
         ]
 
 
@@ -477,39 +478,6 @@ sponsor name src maxWidthPx =
             , css [ width (pct 100), height (px 88) ]
             ]
             []
-        ]
-
-
-siteFooter : Html Msg
-siteFooter =
-    footer
-        [ css Styles.footer.top
-        , css
-            [ lineHeight (num 1.4)
-            , paddingTop (rem 4)
-            ]
-        ]
-        [ div [ css Styles.footer.container ]
-            [ div [ css (Styles.footer.left ++ [ displayFlex, alignItems center ]) ]
-                [ text "© Elm in the Spring 2019"
-                , div [ css [ fontSize (rem 1.5) ] ]
-                    [ a
-                        [ css [ marginLeft (rem 1) ]
-                        , href "https://twitter.com/ElmInTheSpring"
-                        , target "_blank"
-                        ]
-                        [ i [ Attr.class "fa fa-twitter-square" ] []
-                        ]
-                    ]
-                ]
-            , a
-                [ css Styles.footer.right
-                , css [ textAlign right ]
-                , href "https://github.com/ryannhg/elm-in-the-spring"
-                , Attr.target "_blank"
-                ]
-                [ text "This site is open-source and written with Elm!" ]
-            ]
         ]
 
 
---
+++ src/Shared.elm
@@ -0,0 +1,39 @@
+module Shared exposing (siteFooter)
+
+import Css exposing (..)
+import Html.Styled exposing (..)
+import Html.Styled.Attributes as Attr exposing (css, href, target)
+import Styles
+
+
+siteFooter : Html msg
+siteFooter =
+    footer
+        [ css Styles.footer.top
+        , css
+            [ lineHeight (num 1.4)
+            , paddingTop (rem 4)
+            ]
+        ]
+        [ div [ css Styles.footer.container ]
+            [ div [ css (Styles.footer.left ++ [ displayFlex, alignItems center ]) ]
+                [ text "© Elm in the Spring 2019"
+                , div [ css [ fontSize (rem 1.5) ] ]
+                    [ a
+                        [ css [ marginLeft (rem 1) ]
+                        , href "https://twitter.com/ElmInTheSpring"
+                        , target "_blank"
+                        ]
+                        [ i [ Attr.class "fa fa-twitter-square" ] []
+                        ]
+                    ]
+                ]
+            , a
+                [ css Styles.footer.right
+                , css [ textAlign right ]
+                , href "https://github.com/ryannhg/elm-in-the-spring"
+                , Attr.target "_blank"
+                ]
+                [ text "This site is open-source and written with Elm!" ]
+            ]
+        ]
